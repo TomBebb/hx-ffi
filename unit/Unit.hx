e8ad6513b2185ec15394ff5a0ab0d0ec9bfe9411
Tom Bebbington <tombebb@protonmail.com>
2017-12-15 05:16:37 +0000

    Remove Math from test

--- unit/Unit.hx
+++ unit/Unit.hx
@@ -4,7 +4,6 @@ class Unit {
 	static function main() {
 		var r = new Runner();
 		r.addCase(new TestTypes());
-		r.addCase(new TestMath());
 		r.addCase(new TestTests());
 		r.run();
 		Report.create(r);
